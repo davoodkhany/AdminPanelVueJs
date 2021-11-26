6a633e9ed08abb8008dca640ace29494f4b23570
davood <davoodkhany@gmail.com>
2021-11-26 23:41:41 +0330

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SidebarAdminPanel></SidebarAdminPanel>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SidebarAdminPanel from './components//Layouts/SidebarAdminPanel'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+  SidebarAdminPanel
   }
+
 }
 </script>
 
---
+++ src/components/Layouts/SidebarAdminPanel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-border>Toggle Sidebar</b-button>
+    <b-sidebar id="sidebar-border" sidebar-class="border-right border-danger">
+      <div class="px-2 py-1">
+        <p></p>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+
+
+<style>
+
+.collapsed > .when-open,
+.not-collapsed > .when-closed {
+  width: 130px !important;
+}
+.b-sidebar {
+  width: 260px !important;
+}
+
+</style>
